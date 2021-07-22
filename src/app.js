ba368945c5b9c0acd45cd74157001c2a475d87a2
Taylor Elliott <tmikale@gmail.com>
2021-07-22 11:53:22 -0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const fs = require ('fs')
+const path = require ('path')
+const express = require ('express')
+
+const app = express()
+
+app.set(__dirname, path.join('views', './src/views'))
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, '/public/')))
+
+get.route('/')
+    .get((req, res) => {
+        res.render('index, title:')
+    })
+
+app.listen('3000', () => {
+    console.log('PS Project Running on port 3000!')
+})
