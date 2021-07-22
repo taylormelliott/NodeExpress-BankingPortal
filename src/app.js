4093264cdebcfb7454cdd0333db7ed068535746c
Taylor Elliott <tmikale@gmail.com>
2021-07-22 12:57:30 -0600

    second commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require ('express')
 
 const app = express()
 
-app.set(__dirname, path.join('views', './src/views'))
+app.set(__dirname, path.join('views', './views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, '/public/')))
